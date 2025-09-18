2445ba628bb4f1ba23e3ccd88f0d8977fbe465ce
sanju-dev1997 <162017152+sanju-dev1997@users.noreply.github.com>
2025-09-18 10:01:49 +0530

    Update largestConsecutiveSequence.js

--- LC-Arrays-Medium/largestConsecutiveSequence.js
+++ LC-Arrays-Medium/largestConsecutiveSequence.js
@@ -22,5 +22,57 @@ function brute(nums, n) {
 brute(nums, n);
 
 //Better
+//TC - O(NlogN + N)
+//SC - O(1)
+function better(nums,n){
+    if (n == 0) {
+        return 0
+    }
+    nums.sort((a, b) => a - b)
+    let max = 1;
+    let count = 1;
+    let prev = nums[0];
+    for (let i = 0; i < nums.length; i++) {
+        let prevEl = nums[i] - 1;
+        if (nums[i] === prev) {
+            continue
+        }
+        else if (prevEl === prev) {
+            count++;
+            prev++
+            max = Math.max(count, max);
+        } else if (prevEl !== prev) {
+            count = 1;
+            prev = nums[i]
+            max = Math.max(count, max);
+        }
+    }
+    console.log(`Better : ${max}`);
+}
 
 //Optimal
+//TC - O(N+2N)
+//SC - O(N)
+function optimal(nums,n){
+    let obj = {};
+    for (let i = 0; i < n; i++) {
+        if (obj[nums[i]] === undefined) {
+            obj[nums[i]] = 0;
+        }
+    }
+
+    let max = 0;
+
+    for (let key in obj) {
+        if (obj[+key - 1] === undefined) {
+            let streak = 1;
+            let next = +key + 1;
+            while (obj[next] !== undefined) {
+                streak++;
+                next++;
+            }
+            max = Math.max(streak, max);
+        }
+    }
+    console.log(`Optimal : ${max}`);
+}
